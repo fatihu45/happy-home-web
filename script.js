b8ddd2d01dbb15a691d1255089757d06db5eae66
Fatihu45 <fatihu45@gmail.com>
2026-01-06 12:51:35 +0100

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector(".contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for contacting Happy Home School. We will get back to you shortly.");
+  this.reset();
+});
+
+
+// Slider logic
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+nextBtn.addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
+
+prevBtn.addEventListener("click", () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
+
+// Auto slide every 5 seconds
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 5000);
